7c706c21c09cadc63bec79e40f700e009a166599
shira kostantiner <shirah.zilber12@gmail.com>
2017-02-23 15:51:46 +0200

    1

--- Lesson09-java collections/src/com/company/Main.java
+++ Lesson09-java collections/src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	//
+        Queue queue = new Queue();
+        for (int i = 1; i < 4; i++) {
+            queue.insert(i);
+        }
+        queue.pop();
+        queue.insert(15);
+        printQ(queue);
+    }
+    public static void printQ(Queue t){
+        while(!t.isEmpty()) {
+            System.out.print(t.pop()+" ");;
+        }
     }
 }
--- Lesson09-java collections/src/com/company/Queue.java
+++ Lesson09-java collections/src/com/company/Queue.java
@@ -4,33 +4,42 @@ package com.company;
 
 public class Queue {
     int[] arr;
-    int head, tail;
+    int front, rear;
+    int size;
 
     public Queue() {
-        arr = new int[10];
-        head = 0;
-        tail = 0;
+        arr = new int[4];
+        front = 0;
+        rear = arr.length-1;
+        size=0;
+    }
+    public int getSize(){
+
     }
 
-    public void insert(int x) {
-        if (isFull())
-            throw new IndexOutOfBoundsException();
-        arr[tail] = x;
-        tail = (tail % arr.length) + 1;
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public boolean isFull(){
+        return size==arr.length;
     }
 
-    public int pop() {
-        if (isEmpty())
+    public void insert(int x){
+        if(isFull())
+            throw new IndexOutOfBoundsException("יותר מדי אברים ");
+        rear = (rear +1)% arr.length ;
+        arr[rear] = x;
+        size++;
+        //System.out.print(arr[rear]+" ");
+    }
+
+    public int pop(){
+        if(isEmpty())
             throw new IndexOutOfBoundsException();
-        int x = arr[head];
-        head = (head & arr.length) + 1;
+        int x = arr[front];
+        front = (front % arr.length) + 1;
         return x;
     }
 
 
-    public boolean isEmpty() {
-        return head == tail;
-    }
-
-    public boolean isFull() {
-        return tail % (arr.length) + 1 ;
+}
