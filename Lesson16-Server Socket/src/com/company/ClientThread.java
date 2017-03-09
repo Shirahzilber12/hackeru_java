e8544d2669cba8d92aa9177853a26fa39001da80
shira kostantiner <shirah.zilber12@gmail.com>
2017-03-09 12:50:00 +0200

    1

--- Lesson16-Server Socket/src/com/company/ClientThread.java
+++ Lesson16-Server Socket/src/com/company/ClientThread.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 /**
  * Created by hackeru on 3/8/2017.
@@ -16,7 +17,7 @@ public class ClientThread extends Thread {
         this.clientSocket = clientSocket;
     }
 
-    @Override
+ /*   @Override
     public void run() {
         try {
             inputStream = clientSocket.getInputStream();
@@ -26,7 +27,7 @@ public class ClientThread extends Thread {
             String messageFromClient =
                     new String(buffer,0,actuallyRead);
             System.out.println(messageFromClient);
-            outputStream.write("thank you".getBytes());
+            outputStream.write("thank you client".getBytes());
 
 
 
@@ -55,5 +56,98 @@ public class ClientThread extends Thread {
                         e.printStackTrace();
                     }
         }
+    }*/
+   public void run() {
+        try {
+            inputStream = clientSocket.getInputStream();
+            outputStream = clientSocket.getOutputStream();
+            byte[] buffer = new byte[9];
+            byte[] bufferInt = new byte[4];
+            int sum = 0;
+            inputStream.read(buffer);
+            int num1 = ByteBuffer.wrap(buffer).getInt();
+           System.out.println(num1);
+
+            int num2 = ByteBuffer.wrap(buffer,4,4).getInt();
+            System.out.println(num2);
+            char operator = (char)buffer[9];
+           switch (operator){
+               case '+':
+                    sum = sum(num1,num2);
+                   break;
+               case '-':
+                    sum = subtraction(num1,num2);
+                   break;
+               case '*':
+                    sum = mult(num1,num2);
+                   break;
+               case '/':
+                   sum = divide(num1,num2);
+
+                   break;
+               case '%':
+                   sum = mode(num1,num2);
+                   break;
+
+
+           }
+
+            ByteBuffer.wrap(buffer).putInt(sum);
+            outputStream.write(bufferInt);
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (inputStream!=null)
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (outputStream!=null)
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                if (clientSocket!=null)
+                    try {
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+        }
+    }
+    private int sum(int num1 ,int num2){
+       return num1+num2;
+    }
+    private int subtraction(int num1 ,int num2){
+        return num1-num2;
+    }
+    private int divide(int num1 ,int num2){
+        if(num1==0)
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return num1/num2;
+    }
+    private int mode(int num1 ,int num2){
+        if(num1==0)
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return num1%num2;
+    }
+    private int mult(int num1 ,int num2){
+        return num1*num2;
     }
 }
--- Lesson16-Socket Client/src/com/company/Main.java
+++ Lesson16-Socket Client/src/com/company/Main.java
@@ -5,10 +5,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.util.Scanner;
+import java.util.concurrent.Exchanger;
 
 public class Main {
     final static int PORT = 3000;
+
     public static void main(String[] args) {
 	/*try {
         Socket socket = new Socket("127.0.0.1",PORT);
@@ -25,24 +28,65 @@ public class Main {
     } catch (IOException e) {
         e.printStackTrace();
     }*/
-		try {
-        Socket socket = new Socket("127.0.0.1",PORT);
-        InputStream inputStream = socket.getInputStream();
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write("hi server".getBytes());
-        Scanner scanner=new Scanner(System.in);
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        byte[] buffer = new byte[4];
-        int actuallyRead=inputStream.read(buffer  );
-        String responsFromServer = new String(buffer,0,actuallyRead);
-        System.out.println(responsFromServer);
-        inputStream.close();
-        outputStream.close();
-        socket.close();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+        try {
+            Socket socket = new Socket("127.0.0.1", PORT);
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            Scanner scanner = new Scanner(System.in);
+            byte[] buffer = new byte[9];
+            byte[] bufferInt = new byte[4];
+            int i = 2;
+            int[] arr = new int[2];
+
+            System.out.println("please enter operator");
+            // byte[] operator_arr =new byte[1] ;
+            String s =  scanner.next();
+            char operator = s.charAt(0);
+            while((operator!='+')&&( operator!= '-') && (operator!= '*')&& (operator!= '%')&& (operator!= '/'))
+            {
+                System.out.println("you not enter operator, please enter again");
+                s =  scanner.next();
+                operator = s.charAt(0);
+            }
+
+
+            while (i > 0){
+                System.out.println("please enter number");
+                try {
+                    Scanner scanner1 = new Scanner(System.in);
+                    arr[arr.length-i] = scanner1.nextInt();
+                    i--;
+                    if ((i==0)&&(arr[1]== 0)) {
+                        if ((operator == '/')||(operator == '%'))
+                        {
+                            System.out.println("not zero");
+                            i++;
+                        }
+                    }
+                    /*ByteBuffer.wrap(buffer).putInt(num);
+                    outputStream.write(buffer);*/
+                }catch (Exception e){
+                    System.out.println("no num, enter again");
+                }
+            }
+
+            ByteBuffer.wrap(buffer).putInt(arr[0]);
+            ByteBuffer.wrap(buffer,4,4).putInt(arr[1]);
+            ByteBuffer.wrap(buffer,8,1).putInt(operator);
+            outputStream.write(buffer);
+
+
+            System.out.println("your result:");
+            inputStream.read(bufferInt);
+            System.out.println( ByteBuffer.wrap(buffer).getInt());
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+             } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
